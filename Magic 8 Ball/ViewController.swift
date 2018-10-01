3300f65d6d6cd9b7e1f36c9b8c9126c79e2f267e
Milica Sucevic <milicasucevic@gmail.com>
2018-10-01 21:19:12 +0100

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Milica Sucevic on 01/10/2018.
+//  Copyright © 2018 Milica Sucevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
