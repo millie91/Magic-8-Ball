c8d6580d80a6eea866cc81675cc37d17301bf9c3
Milica Sucevic <milicasucevic@gmail.com>
2018-10-01 21:45:12 +0100

    Initial Upload

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomBallNumber : Int = 0
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateButtonImages()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+        updateButtonImages()
+        
+    }
+    
+    func updateButtonImages() {
+        randomBallNumber = Int(arc4random_uniform(5))
+        
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        updateButtonImages()
+        
+    }
+    
 }
 
